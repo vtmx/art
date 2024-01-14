ce84706605f8fc2b53ad66a2f185d0429e932a60
vtmx <vtmx22@gmail.com>
2024-01-14 15:35:01 -0300

    add

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ function updateSizeCanvas(canvas) {
   canvas.height = document.body.clientHeight;
 }
 
+function addController() {
+  const controller = document.createElement('div');
+  controller.setAttribute('id', 'controller');
+  document.body.insertAdjacentElement('afterbegin', controller);
+}
+
 export { createCanvas, updateSizeCanvas };
